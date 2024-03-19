151b318d3613448e0d6e7a8f504cfcc5f2f7565f
Natalia Corea <46870371+nlunaty@users.noreply.github.com>
2024-03-19 16:59:14 +0100

    fix

--- lecciones/Unidad03/leccion_02_valores_dinamicos_window.ts
+++ lecciones/Unidad03/leccion_02_valores_dinamicos_window.ts
@@ -4,13 +4,14 @@
 // capturar y reportar errores en toda la aplicación.
 // Extendiendo el objeto window para el registro global de errores
 interface Window {
-  logError: (error: Error, context?: string) => void;
+  logError: (error: Error, contexto?: string) => void;
 }
 
 // Implementación de la función global de registro de errores
 window.logError = (error, contexto = 'General') => {
   console.log(`Error en ${contexto}:`, error);
-  // Aquí, típicamente enviarías la información del error a un servicio de monitoreo
+  // Aquí, típicamente enviarías la información del error a 
+  // un servicio de monitoreo
 };
 
 // Manejador global de errores
@@ -27,5 +28,7 @@ try {
   // Algún proceso propenso a errores
   throw new Error('Algo salió mal');
 } catch (error) {
-  window.logError(error, 'Función de ejemplo');
+  window.logError(error as Error, 'Función de ejemplo');
 }
+
+throw new Error('Algo salió mal otra vez');
--- lecciones/Unidad03/leccion_06_promesas_typescript.ts
+++ lecciones/Unidad03/leccion_06_promesas_typescript.ts
@@ -1,12 +1,15 @@
-// Ejemplo que simula el proceso de registro en varios pasos utilizando Operaciones
-// Retardadas en Secuencia.
+// Ejemplo que simula el proceso de registro en varios 
+// pasos utilizando operaciones asíncronas en secuencia.
 
-// En este escenario, tenemos una serie de pasos de validación que deben completarse
-// secuencialmente. Después de cada paso, el usuario recibe retroalimentación sobre el
+// En este escenario, tenemos una serie de pasos
+// de validación que deben completarse secuencialmente.
+// En cada paso, el usuario recibe información sobre el
 // progreso.
 
-// Simularemos estos pasos con funciones asíncronas que se resuelven después de un retraso,
-// representando operaciones que consumen tiempo como validaciones en el servidor o
+// Simularemos estos pasos con funciones asíncronas 
+// que se resuelven después de un retraso,
+// representando operaciones que consumen tiempo como 
+// validaciones en el servidor o
 // verificaciones en la base de datos.
 
 type ResultadoPasoValidacion = {
@@ -20,7 +23,9 @@ const simularPasoValidacion = async (
 ): Promise<ResultadoPasoValidacion> => {
   return new Promise(resolver => {
     setTimeout(() => {
-      const esValido = Math.random() > 0.2; // Simula una verificación de validación con un 80% de probabilidades de pasar
+      const esValido = Math.random() > 0.2; 
+      // Simula una verificación de validación con un 
+      // 80% de probabilidades de pasar
       const mensaje = esValido
         ? `Paso ${paso} completado exitosamente.`
         : `Fallo de validación en el paso ${paso}.`;
@@ -31,34 +36,40 @@ const simularPasoValidacion = async (
 };
 
 const validarProcesoRegistro = async (): Promise<void> => {
-  const pasos = [1, 2, 3, 4]; // Representa cada paso del proceso de registro/validación
+  const pasos = [1, 2, 3, 4];
+  // Representa cada paso del proceso de registro/validación
   let todosPasosValidos = true;
 
   for (const paso of pasos) {
     const resultado = await simularPasoValidacion(paso);
     if (!resultado.esValido) {
       todosPasosValidos = false;
-      break; // Detener el proceso de validación si algún paso falla
+      break; // Detener el proceso de validación si algún 
+      // paso falla
     }
   }
 
   if (todosPasosValidos) {
     console.log(
-      'Todos los pasos completados exitosamente. Usuario registrado con éxito.'
+      `Todos los pasos completados exitosamente. 
+      Usuario registrado con éxito.`
     );
-    // Continuar con el siguiente paso en el proceso de registro, por ejemplo, crear la cuenta de usuario
+    // Continuar con el siguiente paso en el proceso de 
+    // registro, por ejemplo, crear la cuenta de usuario
   } else {
     console.log(
-      'Proceso de registro detenido debido a un paso de validación fallido.'
+      `Proceso de registro detenido debido 
+      a un paso de validación fallido.`
     );
-    // Proporcionar retroalimentación al usuario y potencialmente permitirle corregir la entrada
+    // Proporcionar retroalimentación al usuario y 
+    // potencialmente permitirle corregir la entrada
   }
 };
 
 // Invocar el proceso de validación
 validarProcesoRegistro().catch(error =>
   console.error(
-    'El proceso de validación del registro encontró un error:',
+    `El proceso de validación del registro encontró un error:`,
     error
   )
 );
